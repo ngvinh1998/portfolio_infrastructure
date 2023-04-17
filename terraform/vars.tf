92bf5cd506ac45c999d0ef8978a41b55b96ab664
Vinh Nguyen <ngqvinh1998@gmail.com>
2023-04-17 16:49:39 +0700

    infra: add AWS credential in terraform config
    Signed-off-by: Vinh Nguyen <ngqvinh1998@gmail.com>

---
+++ terraform/vars.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+  type        = string
+  sensitive   = true
+  description = "My AWS access key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  sensitive   = true
+  description = "My AWS secret key"
+}
